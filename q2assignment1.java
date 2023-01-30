a24f5aa75e1b56e67af217dd0563d9903ac35687
nishant20t <117446623+nishant20t@users.noreply.github.com>
2023-01-30 22:47:12 +0530

    Add files via upload

---
+++ q2assignment1.java
@@ -0,0 +1,16 @@
+
+public class q2assignment1 {
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<n;i++)
+		{
+			for(int j=0;j<(n);j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+			
+	}
+}
